17117995d83ee751f1fd165422f48e8673bcd84a
Paweł Giełdoń <93713186+PawelGieldon@users.noreply.github.com>
2022-05-15 13:45:02 +0200

    Add files via upload

---
+++ Projekt.NET/Projekt.NET/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace Projekt.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.X1 = new System.Windows.Forms.Button();
+            this.X2 = new System.Windows.Forms.Button();
+            this.Y2 = new System.Windows.Forms.Button();
+            this.Y3 = new System.Windows.Forms.Button();
+            this.Z2 = new System.Windows.Forms.Button();
+            this.Z1 = new System.Windows.Forms.Button();
+            this.Y1 = new System.Windows.Forms.Button();
+            this.X3 = new System.Windows.Forms.Button();
+            this.Z3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetScoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.X_count = new System.Windows.Forms.Label();
+            this.O_count = new System.Windows.Forms.Label();
+            this.whosturn = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // X1
+            // 
+            this.X1.BackColor = System.Drawing.SystemColors.Info;
+            this.X1.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.X1.Location = new System.Drawing.Point(26, 69);
+            this.X1.Name = "X1";
+            this.X1.Size = new System.Drawing.Size(82, 82);
+            this.X1.TabIndex = 3;
+            this.X1.UseVisualStyleBackColor = false;
+            this.X1.Click += new System.EventHandler(this.Click);
+            // 
+            // X2
+            // 
+            this.X2.BackColor = System.Drawing.SystemColors.Info;
+            this.X2.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.X2.Location = new System.Drawing.Point(137, 69);
+            this.X2.Name = "X2";
+            this.X2.Size = new System.Drawing.Size(82, 82);
+            this.X2.TabIndex = 4;
+            this.X2.UseVisualStyleBackColor = false;
+            this.X2.Click += new System.EventHandler(this.Click);
+            // 
+            // Y2
+            // 
+            this.Y2.BackColor = System.Drawing.SystemColors.Info;
+            this.Y2.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Y2.Location = new System.Drawing.Point(137, 168);
+            this.Y2.Name = "Y2";
+            this.Y2.Size = new System.Drawing.Size(82, 82);
+            this.Y2.TabIndex = 5;
+            this.Y2.UseVisualStyleBackColor = false;
+            this.Y2.Click += new System.EventHandler(this.Click);
+            // 
+            // Y3
+            // 
+            this.Y3.BackColor = System.Drawing.SystemColors.Info;
+            this.Y3.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Y3.Location = new System.Drawing.Point(247, 168);
+            this.Y3.Name = "Y3";
+            this.Y3.Size = new System.Drawing.Size(82, 82);
+            this.Y3.TabIndex = 6;
+            this.Y3.UseVisualStyleBackColor = false;
+            this.Y3.Click += new System.EventHandler(this.Click);
+            // 
+            // Z2
+            // 
+            this.Z2.BackColor = System.Drawing.SystemColors.Info;
+            this.Z2.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Z2.Location = new System.Drawing.Point(137, 267);
+            this.Z2.Name = "Z2";
+            this.Z2.Size = new System.Drawing.Size(82, 82);
+            this.Z2.TabIndex = 7;
+            this.Z2.UseVisualStyleBackColor = false;
+            this.Z2.Click += new System.EventHandler(this.Click);
+            // 
+            // Z1
+            // 
+            this.Z1.BackColor = System.Drawing.SystemColors.Info;
+            this.Z1.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Z1.Location = new System.Drawing.Point(26, 267);
+            this.Z1.Name = "Z1";
+            this.Z1.Size = new System.Drawing.Size(82, 82);
+            this.Z1.TabIndex = 8;
+            this.Z1.UseVisualStyleBackColor = false;
+            this.Z1.Click += new System.EventHandler(this.Click);
+            // 
+            // Y1
+            // 
+            this.Y1.BackColor = System.Drawing.SystemColors.Info;
+            this.Y1.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Y1.Location = new System.Drawing.Point(26, 168);
+            this.Y1.Name = "Y1";
+            this.Y1.Size = new System.Drawing.Size(82, 82);
+            this.Y1.TabIndex = 9;
+            this.Y1.UseVisualStyleBackColor = false;
+            this.Y1.Click += new System.EventHandler(this.Click);
+            // 
+            // X3
+            // 
+            this.X3.BackColor = System.Drawing.SystemColors.Info;
+            this.X3.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.X3.Location = new System.Drawing.Point(247, 69);
+            this.X3.Name = "X3";
+            this.X3.Size = new System.Drawing.Size(82, 82);
+            this.X3.TabIndex = 10;
+            this.X3.UseVisualStyleBackColor = false;
+            this.X3.Click += new System.EventHandler(this.Click);
+            // 
+            // Z3
+            // 
+            this.Z3.BackColor = System.Drawing.SystemColors.Info;
+            this.Z3.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Z3.Location = new System.Drawing.Point(247, 267);
+            this.Z3.Name = "Z3";
+            this.Z3.Size = new System.Drawing.Size(82, 82);
+            this.Z3.TabIndex = 11;
+            this.Z3.UseVisualStyleBackColor = false;
+            this.Z3.Click += new System.EventHandler(this.Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.resetScoreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(501, 30);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.newGameToolStripMenuItem.ForeColor = System.Drawing.Color.Green;
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(113, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(55, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // resetScoreToolStripMenuItem
+            // 
+            this.resetScoreToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.resetScoreToolStripMenuItem.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.resetScoreToolStripMenuItem.Name = "resetScoreToolStripMenuItem";
+            this.resetScoreToolStripMenuItem.Size = new System.Drawing.Size(113, 26);
+            this.resetScoreToolStripMenuItem.Text = "Reset Score";
+            this.resetScoreToolStripMenuItem.Click += new System.EventHandler(this.resetScoreToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(371, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 21);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "X Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(370, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 21);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "O Score:";
+            // 
+            // X_count
+            // 
+            this.X_count.AutoSize = true;
+            this.X_count.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.X_count.Location = new System.Drawing.Point(442, 286);
+            this.X_count.Name = "X_count";
+            this.X_count.Size = new System.Drawing.Size(17, 19);
+            this.X_count.TabIndex = 15;
+            this.X_count.Text = "0";
+            this.X_count.Click += new System.EventHandler(this.X_count_Click);
+            // 
+            // O_count
+            // 
+            this.O_count.AutoSize = true;
+            this.O_count.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.O_count.Location = new System.Drawing.Point(442, 330);
+            this.O_count.Name = "O_count";
+            this.O_count.Size = new System.Drawing.Size(17, 19);
+            this.O_count.TabIndex = 16;
+            this.O_count.Text = "0";
+            // 
+            // whosturn
+            // 
+            this.whosturn.AutoSize = true;
+            this.whosturn.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.whosturn.Location = new System.Drawing.Point(386, 104);
+            this.whosturn.Name = "whosturn";
+            this.whosturn.Size = new System.Drawing.Size(0, 21);
+            this.whosturn.TabIndex = 17;
+            this.whosturn.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(501, 381);
+            this.Controls.Add(this.whosturn);
+            this.Controls.Add(this.O_count);
+            this.Controls.Add(this.X_count);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Z3);
+            this.Controls.Add(this.X3);
+            this.Controls.Add(this.Y1);
+            this.Controls.Add(this.Z1);
+            this.Controls.Add(this.Z2);
+            this.Controls.Add(this.Y3);
+            this.Controls.Add(this.Y2);
+            this.Controls.Add(this.X2);
+            this.Controls.Add(this.X1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kółko i Krzyżyk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button X1;
+        private System.Windows.Forms.Button X2;
+        private System.Windows.Forms.Button Y2;
+        private System.Windows.Forms.Button Y3;
+        private System.Windows.Forms.Button Z2;
+        private System.Windows.Forms.Button Z1;
+        private System.Windows.Forms.Button Y1;
+        private System.Windows.Forms.Button X3;
+        private System.Windows.Forms.Button Z3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label X_count;
+        private System.Windows.Forms.Label O_count;
+        private System.Windows.Forms.Label whosturn;
+        private System.Windows.Forms.ToolStripMenuItem resetScoreToolStripMenuItem;
+    }
+}
+
---
+++ Projekt.NET/Projekt.NET/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt.NET
+{
+    public partial class Form1 : Form
+    {
+
+        bool round = true;
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (round)
+            {
+                whosturn.Text = "X Turn";
+            }
+            else
+            {
+                whosturn.Text = "O Turn";
+            }
+
+        }
+
+        
+
+        private void Click(object sender, EventArgs e)
+        {
+            Button a = (Button)sender;
+            if (round)
+            {
+                a.Text = "X";
+            }
+            else
+            {
+                a.Text = "O";
+            }
+            a.Enabled = false;          
+            round = !round;
+            count++;
+            Checking();
+        }
+
+        private void Checking()
+        {
+            bool winner = false;
+
+            if (round)
+            {
+                whosturn.Text = "X Turn";
+            }
+            else
+            {
+                whosturn.Text = "O Turn";
+            }
+
+
+
+            if ((X1.Text == X2.Text) && (X2.Text == X3.Text) && (!X1.Enabled))
+            {
+                winner = true;
+            }
+
+            else if ((Y1.Text == Y2.Text) && (Y2.Text == Y3.Text) && (!Y1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((Z1.Text == Z2.Text) && (Z2.Text == Z3.Text) && (!Z1.Enabled))
+            {
+                winner = true;
+            }
+
+            else if ((X1.Text == Y1.Text) && (Y1.Text == Z1.Text) && (!X1.Enabled))
+            {
+                winner = true;
+            }
+
+            else if ((X2.Text == Y2.Text) && (Y2.Text == Z2.Text) && (!X2.Enabled))
+            {
+                winner = true;
+            }
+            else if ((X3.Text == Y3.Text) && (Y3.Text == Z3.Text) && (!X3.Enabled))
+            {
+                winner = true;
+            }
+
+
+            else if ((X1.Text == Y2.Text) && (Y2.Text == Z3.Text) && (!X1.Enabled))
+            {
+                winner = true;
+            }
+
+            else if ((X3.Text == Y2.Text) && (Y2.Text == Z1.Text) && (!Z1.Enabled))
+            {
+                winner = true;
+            }
+            
+
+
+
+            if (winner)
+            {
+                Done();
+                String win = "";
+                if (round)
+                {
+
+                    win = " O ";
+                    O_count.Text = (Int32.Parse(O_count.Text) + 1).ToString();
+                    
+                }
+                else
+                {
+                    win = " X ";
+                    X_count.Text = (Int32.Parse(X_count.Text) + 1).ToString();
+                    
+                }
+                MessageBox.Show("Gratulację! wygrał" + win);
+                
+
+
+            }
+
+            else
+            {
+                if(count == 9)
+                {
+                    MessageBox.Show("Remis!");
+                }
+            }
+
+
+        }
+
+
+        private void Done()
+        {
+            
+                foreach (Control c in Controls)
+                try
+                {
+                    {
+                    Button a = (Button)c;
+                    a.Enabled = false;
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            round = true;
+
+
+            if (round)
+            {
+                whosturn.Text = "X Turn";
+            }
+            else
+            {
+                whosturn.Text = "O Turn";
+            }
+
+
+
+            foreach (Control c in Controls)
+                try
+                {
+
+                    {
+
+                        Button a = (Button)c;
+                        a.Enabled = true;
+                        a.Text = "";
+                    }
+                }
+                catch
+                {
+
+                }
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void X_count_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetScoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            X_count.Text = "0";
+            O_count.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
